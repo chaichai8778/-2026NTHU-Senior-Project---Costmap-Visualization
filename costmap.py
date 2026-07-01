669dbad4bfc4e8d74ba2df93c322a307437f2eb8
chaichai8778 <popo3q12345@gmail.com>
2026-07-01 17:55:59 +0800

    obstacle map

--- costmap.py
+++ costmap.py
@@ -4,23 +4,34 @@ def obstacle_layer(car_x, car_y, obs_x, obs_y, obstacle_map):
     dx = obs_x - car_x
     dy = obs_y - car_y
     distance = math.sqrt(dx**2 + dy**2)
-    
+
     if distance == 0:
-        return
-        
+        return []
+    
+    modified_points = []
+
     ux = dx / distance
     uy = dy / distance
     
     step = 0.5
     current_dist = 0.0
+
+    # Space point
     while current_dist < distance - 0.2: 
-        curr_x = car_x + ux * current_dist
-        curr_y = car_y + uy * current_dist
+        curr_x = math.floor(car_x + ux * current_dist)
+        curr_y = math.floor(car_y + uy * current_dist)
         
-        obstacle_map[(math.floor(curr_x), math.floor(curr_y))] = 0
+        obstacle_map[(curr_x, curr_y)] = 0
+        modified_points.append(((curr_x, curr_y), 0))
         current_dist += step
 
-    obstacle_map[(math.floor(obs_x), math.floor(obs_y))] = 1
+    # Obstacle point
+    obs_floor_x = math.floor(obs_x)
+    obs_floor_y = math.floor(obs_y)
+    obstacle_map[(obs_floor_x, obs_floor_y)] = 1
+    modified_points.append(((obs_floor_x, obs_floor_y), 1))
+    
+    return modified_points
 
 def inflation_layer(obstacle_map, inflation_radius):
     inflated_map = {}
